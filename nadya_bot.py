c70cad2ec5764da8b48c67b26b36a312f244ead0
Глеб Новиков <novikov_g@gk-tula.local>
2020-10-01 12:36:16 +0300

    added any comments

--- nadya_bot.py
+++ nadya_bot.py
@@ -2,9 +2,11 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 updater = Updater(token='1145342159:AAEwfxOCLaQleZtlsJ4X29KE_37MP_qzPSU', use_context=True)
 dispatcher = updater.dispatcher
+
+# логирование
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
+# 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
