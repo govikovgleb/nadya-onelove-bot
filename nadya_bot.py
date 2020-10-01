ba7de92c57aef6c45217b22e02ba1422a2f38370
Глеб Новиков <novikov_g@gk-tula.local>
2020-10-01 19:13:50 +0300

    add test inlineKB

--- nadya_bot.py
+++ nadya_bot.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 updater = Updater(token='1145342159:AAEwfxOCLaQleZtlsJ4X29KE_37MP_qzPSU', use_context=True)
 dispatcher = updater.dispatcher
 
@@ -8,7 +10,8 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Test btn", callback_data='1')]]) 
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I love you!", reply_markup=reply_markup)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
