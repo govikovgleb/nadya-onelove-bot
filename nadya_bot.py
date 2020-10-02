a8801a1e1cb8992b285a4a21a548a9ada2b53294
Глеб Новиков <novikov_g@gk-tula.local>
2020-10-02 15:18:02 +0300

    add invalid alert message

--- nadya_bot.py
+++ nadya_bot.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import CallbackQueryHandler
 from telegram import InlineKeyboardButton
 from telegram import InlineKeyboardMarkup
 updater = Updater(token='1145342159:AAEwfxOCLaQleZtlsJ4X29KE_37MP_qzPSU', use_context=True)
@@ -10,10 +11,14 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # 
 def start(update, context):
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("\u2764", callback_data='1')]]) 
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("\u2764 W-o-W", callback_data='like')]]) 
     context.bot.send_message(chat_id=update.effective_chat.id, text="I love you!", reply_markup=reply_markup)
+def btn(update, context):
+    context.bot.answer_callback_query(callback_query_id=update.callback_data.id, text="You are beautiful!", show_alert=True)
 
 start_handler = CommandHandler('start', start)
+inline_btn_handler = CallbackQueryHandler(btn)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(inline_btn_handler)
 
 updater.start_polling()
