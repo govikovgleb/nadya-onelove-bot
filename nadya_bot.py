c27351d1919ea009b3cf917dc86a7794412684e3
Глеб Новиков <novikov_g@gk-tula.local>
2020-10-02 17:54:14 +0300

    fix invalid mirror message

--- nadya_bot.py
+++ nadya_bot.py
@@ -11,19 +11,22 @@ dispatcher = updater.dispatcher
 # логирование
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
- 
-def start(update, context):
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("\u2764", callback_data='like')]])    
+
+reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("\u2764", callback_data='like')]])
+def start(update, context):        
     context.bot.send_message(chat_id=update.effective_chat.id, text="I love you!", reply_markup=reply_markup)
 def btn(update, context):
     context.bot.answer_callback_query(callback_query_id=update.callback_query.id, text="You are beautiful!", show_alert=True)
-def mirror(update, context):    
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+def mirror(update, context):
+    print(update.message)
+    if update.message.text != '/start':       
+        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text, reply_markup=reply_markup)
+        pass
 
-#start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('start', start)
 mirror_message = MessageHandler(Filters.all, mirror)
 inline_btn_handler = CallbackQueryHandler(btn)
-#dispatcher.add_handler(start_handler)
+dispatcher.add_handler(start_handler)
 dispatcher.add_handler(inline_btn_handler)
 dispatcher.add_handler(mirror_message)
 
