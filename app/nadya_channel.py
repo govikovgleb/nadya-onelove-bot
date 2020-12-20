12fe700479553da48fe44b788c8877e2730d85fd
govikovgleb <novikov.gleb.1204@gmail.com>
2020-12-21 01:39:48 +0300

    fix InlineKeyboardButton

--- app/nadya_channel.py
+++ app/nadya_channel.py
@@ -7,8 +7,8 @@ from app.storage import get_likes_count_by_post, increase_likes_count_by_post
 import logging
 import os
 
-HEART_SYMBOL = "\u2764"
-reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(HEART_SYMBOL)]])
+HEART_SYMBOL = "❤️"
+reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(HEART_SYMBOL, callback_data=1)]])
 msggen_by_user_id = {}
 
 def get_msggen_by_user_id(user_id):
